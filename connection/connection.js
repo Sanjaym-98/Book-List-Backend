9f6803599781bed4e8c0ba4d77582df6c07fa259
Sanjay <sanjaym1998@gmail.com>
2023-03-31 11:49:15 +0530

    updated atlas link

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,9 @@
 const mongoose =require("mongoose");
+const URL = "mongodb+srv://sanjaym1998:EhMHug730tnWkKCB@cluster0.jao4mrc.mongodb.net/Booklist?retryWrites=true&w=majority";
 
 async function myfunc(){
-    await mongoose.connect('mongodb://localhost/bookapp');
+    await mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology:true})
+
 console.log("connected to db")
 }
 
